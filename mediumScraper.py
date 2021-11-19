f7066d93235ff9d693cb003112a7045cacb92673
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-18 17:13:18 -0800

    working

--- mediumScraper.py
+++ mediumScraper.py
@@ -12,7 +12,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-df = pd.DataFrame(columns=["headline", "section1", "section2", "section3", "section4", "section5", "section6", "section7", "section8", "section9", "section10", "section11", "section12", "section13", "section14", "section15", "section16", "section17", "section18", "section19", "section20", "section21", "section22", "section23", "section24", "section25", "section26", "section27", "section28", "section29", "section30", "section31", "section32", "section33", "section34", "section35", "section36", "section37", "section38", "section39", "section40", "section41", "section42", "section43", "section44", "section45", "section46", "section47", "section48", "section49", "section50", "section51", "section52", "section53", "section54", "section55", "section56", "section57", "section58", "section59", "section60", "section61", "section62", "section63", "section64", "section65", "section66", "section67", "section68", "section69", "section70", "section71", "section72", "section73", "section74", "section75", "section76", "section77", "section78", "section79", "section80", "section81", "section82", "section83", "section84", "section85", "section86", "section87", "section88", "section89", "section90", "section91", "section92", "section93", "section94", "section95", "section96", "section97", "section98", "section99", "section100"])
+#df = pd.DataFrame(columns=["headline", "section1", "section2", "section3", "section4", "section5", "section6", "section7", "section8", "section9", "section10", "section11", "section12", "section13", "section14", "section15", "section16", "section17", "section18", "section19", "section20", "section21", "section22", "section23", "section24", "section25", "section26", "section27", "section28", "section29", "section30", "section31", "section32", "section33", "section34", "section35", "section36", "section37", "section38", "section39", "section40", "section41", "section42", "section43", "section44", "section45", "section46", "section47", "section48", "section49", "section50", "section51", "section52", "section53", "section54", "section55", "section56", "section57", "section58", "section59", "section60", "section61", "section62", "section63", "section64", "section65", "section66", "section67", "section68", "section69", "section70", "section71", "section72", "section73", "section74", "section75", "section76", "section77", "section78", "section79", "section80", "section81", "section82", "section83", "section84", "section85", "section86", "section87", "section88", "section89", "section90", "section91", "section92", "section93", "section94", "section95", "section96", "section97", "section98", "section99", "section100"])
+df = pd.DataFrame(columns=["headline", "desc", "writer", "where", "date"])
 
 chrome_options = webdriver.ChromeOptions()
 #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -31,17 +32,64 @@ driver.set_window_size(x, y)
 driver.delete_all_cookies()
 url = "https://medium.com/topic/popular"
 
+def write_csv(header, data, path, mode):
+    with open(path, mode, encoding="utf-8") as f:
+        writer = csv.writer(f)
+        if mode == "w":
+            writer.writerow(header)
+        writer.writerows(data)
+driver.get(url)
+time.sleep(5)
 for i in range(3):
-    driver.get(url)
-    time.sleep(5)
-    headlineEles = driver.find_elements(By.XPATH, "//section/div/div/div/h3/a")
-    h = headlineEles[i]
-    df.at[i+1, 0] = h.text
-    h.click()
-    time.sleep(5)
-    sectionEles = driver.find_elements(By.XPATH, "//section")
-    for j, s in enumerate(sectionEles, start=1):
-        df.at[i+1, j] = s.text
+    elements = driver.find_elements(By.XPATH, "//section")
+    driver.execute_script("arguments[0].scrollIntoView();", elements[-1])
+    time.sleep(2)
+
+headlineEles = driver.find_elements(By.XPATH, "//section/div/div/div/h3/a")
 
 
-df.to_csv('dataMedium.csv', sep=',', encoding='utf-8')
+for i, h in enumerate(headlineEles, start=1):
+    try:
+        desc = h.find_element(By.XPATH, "../following-sibling::div/h3/a")
+        desctext = desc.text
+    except:
+        desctext = ""
+    writerSource = h.find_elements(By.XPATH, "../../../following-sibling::div/div/div/div/a")
+    try:
+        writer = writerSource[0]
+        writertext = writer.text
+    except:
+        writertext = ""
+    try:
+        source = writerSource[1]
+        sourcetext = source.text
+    except:
+        sourcetext = ""
+    try:
+        date = h.find_element(By.XPATH, "../../../following-sibling::div/div/div/div/div/div")
+        datetext = date.text
+    except:
+        datetext = ""
+    print(desc.text)
+    mode = "w" if i == 1 else "a"
+    write_csv(
+        header=["headline", "desc", "writer", "where", "date"],
+        data=zip([h.text], [desctext], [writertext], [sourcetext], [datetext]),
+        path=os.path.join("medDATA.csv"),
+        mode=mode,
+    )
+#for i in range(3):
+#    driver.get(url)
+#    time.sleep(5)
+#    headlineEles = driver.find_elements(By.XPATH, "//section/div/div/div/h3/a")
+#    h = headlineEles[i]
+#    df.at[i+1, 0] = h.text
+#    h.click()
+#    time.sleep(5)
+#    sectionEles = driver.find_elements(By.XPATH, "//section")
+#    for j, s in enumerate(sectionEles, start=1):
+#        df.at[i+1, j] = s.text
+
+
+
+#df.to_csv('dataMedium.csv', sep=',', encoding='utf-8')
